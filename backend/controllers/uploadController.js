dd6e3e021ed3bfa72f7297738050b1f73a6c8daa
asrar000 <asrarahmedcell000@gmail.com>
2023-04-08 00:06:29 +0600

    Backend

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -1,29 +1,32 @@
-const uploadController = require('express').Router()
+const uploadController=require("express").Router()
+const multer=require('multer')
+const{verifyToken}=require("../middlewares/verifyToken")
 
-const multer = require('multer')
-const {verifyToken} = require('../middlewares/verifyToken')
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public/images")
+const storage=multer.diskStorage({
+    destination:(req,file,cb) =>{
+        cb(null,"public/images")
     },
-    filename: (req, file, cb) => {
-        cb(null, req.body.filename)
+    filename:(req,file,cb) => {
+        cb(null,req.body.filename)
+
     }
 })
 
-const upload = multer({
+const upload=multer({
     storage
-    // same as storage: storage
+    //same as storage:storage
 })
 
-// req.body.image
-uploadController.post('/image', verifyToken, upload.single('image'), (req, res) => {
-  try {
-    return res.status(201).json({msg: "Successfully uploaded file"})
-  } catch (error) {
-    console.error(error.message)
-  }
+uploadController.post("/image",verifyToken,upload.single('image'),(req,res) => {
+     try{
+        return res.status(201).json({msg:"Sucessfully uploaded file"})
+
+
+     }catch(error){
+        console.error(error.message)
+
+     }
+
 })
 
-module.exports = uploadController
+module.exports=uploadController
