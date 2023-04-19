65000a03254bc1503881a981b08c941597b89af7
asrar000 <asrarahmedcell000@gmail.com>
2023-04-19 09:24:53 +0600

    Client

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import Hero from '../hero/Hero'
 import classes from './home.module.css'
+import illustration1 from '../../assets/male-delivery-guy-riding-scooter.svg'
+import illustration2 from '../../assets/delivery-location.svg'
+import illustration3 from '../../assets/deliveryman-with-pizza.svg'
+import Foods from '../foods/Foods'
+import Newsletter from '../newsletter/Newsletter'
 
-const Home= () => {
+const Home = () => {
   return (
     <div className={classes.container}>
       <div className={classes.wrapper}>
-      <Hero />
+        <Hero />
+        <div className={classes.delivery}>
+          <div className={classes.titles}>
+            
+          </div>
+          <Foods />
+          <Newsletter />
+          <div className={classes.deliveryInfos}>
+            <div className={classes.deliveryInfo}>
+              <img src={illustration1} alt="" className={classes.firstImg}/>
+              <h3>Our delivery guy is always on time</h3>
+            </div>
+            <div className={classes.deliveryInfo}>
+              <img src={illustration2} alt="" className={classes.secondImg}/>
+              <h3>He works very hard</h3>
+            </div>
+            <div className={classes.deliveryInfo}>
+              <img src={illustration3} alt="" className={classes.thirdImg}/>
+              <h3>He is friendly and social</h3>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
   )
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import classes from './navbar.module.css'
 import {AiOutlineUser,AiOutlineShoppingCart} from 'react-icons/ai'
+import { useState } from 'react'
+//import { useDispatch, useSelector } from 'react-redux'
+//import { logout } from '../../redux/authSlice'
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+
+
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true)
+    return () => (window.onscroll = null)
+  }
+
+
+
+
   return (
-    <div className={classes.container}>
+    <div className={`${classes.container} ${isScrolled && classes.scrolled}`}>
        <div className={classes.wrapper}>
         <div className={classes.left}>
         <Link to='/' className={classes.title}>
